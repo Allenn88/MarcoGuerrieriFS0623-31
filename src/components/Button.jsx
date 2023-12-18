95e0d6ed909914c6e1d518d0074d3cf2cb9e4c25
allenn <marco.guerrieri@live.it>
2023-12-18 17:07:30 +0100

    esercizioreact

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+
+const Button1 = (props) =>{
+    return <button type="button" onClick={() => console.log("io sono il bottone superiore")}></button>
+}
+export default Button1;
---
+++ src/components/Img.jsx
@@ -0,0 +1,4 @@
+const Img = (props) => {
+    return <img src="https://it.wikipedia.org/wiki/Piaggio_Ciao#/media/File:Ciao_PX.jpg" alt="ciao"></img>
+}
+export default Img;
